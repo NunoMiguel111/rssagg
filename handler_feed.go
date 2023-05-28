934c9c558c8f05f7d19b35e7548e057188152122
Nuno Miguel <up201806434@fe.up.pt>
2023-05-28 09:31:26 +0100

    created post feed with middleware

---
+++ handler_feed.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"time"
+
+	"github.com/NunoMiguel111/rssagg/internal/database"
+	"github.com/google/uuid"
+)
+
+func (cfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		Name string `json:"name"`
+		URL string`json:"url"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+
+	user, err := cfg.DB.CreateFeed(r.Context(), database.CreateUserParams, ){
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create user")
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, databaseUserToUser(user))
+}
